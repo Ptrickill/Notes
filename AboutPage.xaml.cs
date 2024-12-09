2effe5289efa025ee552185c19fde0398343d59f
ptrickill <daniel.diaz.mosquera8@gmail.com>
2024-12-09 18:32:48 -0500

    Agregar archivos de proyecto.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
